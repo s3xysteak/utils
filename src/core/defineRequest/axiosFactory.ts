60b3a98248de635393a352eb89fe0d4170a23853
s3xysteak <j.yulia@qq.com>
2024-03-15 17:57:32 +0800

    docs: improve function docs.

--- src/core/defineRequest/axiosFactory.ts
+++ src/core/defineRequest/axiosFactory.ts
@@ -15,6 +15,42 @@ export type DefineRequestReturns<Options extends object = object> = {
     : (params: any, config: AxiosRequestConfig) => Promise<AxiosResponse>
 }
 
+/**
+ * Define a request factory for axios.
+ *
+ * @example
+ * ```js
+ * import axiosInstance from '@/api'
+ *
+ * const defineRequest = defineRequestAxiosFactory(axiosInstance)
+ *
+ * const api = defineRequest({
+ *   // Start with `get` will use `get` method, otherwise use `post` method.
+ *   getUser: '/user/list',
+ *
+ *   editUser: '/user/edit',
+ *
+ *   // Custom axios method.
+ *   updatePicture: ['/user/picture', 'postForm'],
+ *
+ *   // Custom Request, which will call the function you defined.
+ *   customFunc: (params, config) => axios.post('/user/custom', params, config),
+ * })
+ *
+ * const use = async () => {
+ *   // `get` Request
+ *   const resGet = await api.getUser({ id: 1})
+ *
+ *   // `post` Request
+ *   const resPost = await api.editUser({ id: 1, name: 'newName' })
+ *
+ *   // `postForm` Request
+ *   const resPostForm = await api.updatePicture({ file: 'file' })
+ *
+ *   const resCustom = await api.customFunc({ id: 1 })
+ * }
+ * ```
+ */
 export function defineRequestAxiosFactory(request: AxiosInstance) {
   // TODO: Better Type declaration like `defineProps`
   return <T extends DefineRequestOptions = DefineRequestOptions>(options: T) => {
--- src/core/fp/index.ts
+++ src/core/fp/index.ts
@@ -4,6 +4,18 @@ export type LastReturnType<T extends any[]> =
     T extends [...infer _, infer L] ?
       L extends (...args: any) => infer R ? R : never : never
 
+/**
+ * Compose functions from left to right, with **AWESOME** type intelligent.
+ *
+ * @example
+ * ```js
+ * const add = (a, b) => a + b
+ * const square = n => n * n
+ *
+ * const val = flow(add, square)
+ * val(1, 2) // 9
+ * ```
+ */
 export function flow<T extends ((...args: any[]) => any)[]>(...funcs: T) {
   return (...args: Parameters<T[0]>): LastReturnType<T> =>
     (
