2e9ed017a5210d07665c977c5cedef17455bf877
s3xysteak <j.yulia@qq.com>
2024-05-12 21:10:07 +0800

    feat: new Function `isPromise`.

--- src/core/is.ts
+++ src/core/is.ts
@@ -1,1 +1,11 @@
+import { isFunction, isObject } from '@antfu/utils'
+
 export const isArray = Array.isArray
+
+export function isPromise<T = any>(val: unknown): val is Promise<T> {
+  return (
+    (isObject(val) || isFunction(val))
+    && isFunction((val as any).then)
+    && isFunction((val as any).catch)
+  )
+}
