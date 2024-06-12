293228727f20409eb3574f34247fe104ffc590af
s3xysteak <j.yulia@qq.com>
2024-06-12 11:55:59 +0800

    fix(is): improve `isPromise`.

--- src/core/is.ts
+++ src/core/is.ts
@@ -1,11 +1,7 @@
-import { isFunction, isObject } from '@antfu/utils'
+import { toString } from '@antfu/utils'
 
 export const isArray = Array.isArray
 
-export function isPromise<T = any>(val: unknown): val is Promise<T> {
-  return (
-    (isObject(val) || isFunction(val))
-    && isFunction((val as any).then)
-    && isFunction((val as any).catch)
-  )
+export function isPromise(val: unknown): val is Promise<unknown> {
+  return toString(val) === '[object Promise]'
 }
