7e3dbd3fda9fd728607daf48895dae0930f72100
s3xysteak <86149525+s3xysteak@users.noreply.github.com>
2024-07-07 19:22:54 +0800

    test: add test for base:`createMeta`

--- src/core/base.test.ts
+++ src/core/base.test.ts
@@ -1,10 +1,37 @@
-import { describe, expect, it } from 'vitest'
-import { toLF } from '..'
+import { describe, expect, expectTypeOf, it } from 'vitest'
+import { createMeta, toLF } from './base'
 
 describe('base', () => {
   it('toLF', () => {
     const crlf = 'hello\r\nworld'
     const lf = 'hello\nworld'
     expect(toLF(crlf)).toBe(lf)
+  })
+})
+
+describe('base:createMeta', () => {
+  it('basic', () => {
+    const obj = { one: 1 }
+
+    const [s, g] = createMeta()
+
+    s(obj, { two: 2 })
+    expect(g(obj)).toEqual({ two: 2 })
+
+    s(obj, 2)
+    expect(g(obj)).toBe(2)
+  })
+
+  it('types', () => {
+    const obj = { one: 1 }
+
+    interface Options {
+      one: number
+    }
+    const [s, g] = createMeta<Options>()
+
+    s(obj, { one: 1 })
+    expectTypeOf(s).parameter(1).toEqualTypeOf<Options>()
+    expectTypeOf(g(obj)).toEqualTypeOf<Options>()
   })
 })
