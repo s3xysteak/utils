9cb1cb1ce515343f5520352219619d89491c1d53
s3xysteak <s3xysteak@outlook.com>
2024-09-19 16:56:51 +0800

    feat!: remove `flow`, introduce `pipe` (from rxjs)

--- src/core/fp.test.ts
+++ src/core/fp.test.ts
@@ -1,33 +1,12 @@
-import { describe, expect, expectTypeOf, it } from 'vitest'
-import { flow } from '..'
+import { describe, expect, it } from 'vitest'
+import { pipe } from '..'
 
-describe('flow', () => {
-  it('should work with one func', () => {
-    const add = (a: number, b: number) => a + b
-
-    const val = flow(add)
-    expectTypeOf(val).toEqualTypeOf<typeof add>()
-    expect(val(1, 2)).toBe(3)
-  })
-
-  it('should work with two funcs', () => {
-    const add = (a: number, b: number) => a + b
-    const square = (n: number) => `${n * n}`
-
-    const val = flow(add, square)
-
-    expectTypeOf(val).toEqualTypeOf<(...args: Parameters<typeof add>) => ReturnType<typeof square>>()
-    expect(val(1, 2)).toBe('9')
-  })
-
-  it('should work with three funcs', () => {
-    const add = (a: number, b: number) => a + b
-    const square = (n: number) => `${n * n}`
-    const toStr = (n: string) => `The result is ${n}`
-
-    const val = flow(add, square, toStr)
-
-    expectTypeOf(val).toEqualTypeOf<(...args: Parameters<typeof add>) => ReturnType<typeof toStr>>()
-    expect(val(1, 2)).toBe('The result is 9')
+describe('fp', () => {
+  it('pipe', () => {
+    const fn = pipe(
+      (v: number) => v + 1,
+      v => v * 2,
+    )
+    expect(fn(1)).toBe(4)
   })
 })
