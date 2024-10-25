2a0d527a99a6da2aeb94c50bddf298a7d8331cc7
s3xysteak <s3xysteak@outlook.com>
2024-10-25 21:56:04 +0800

    chore: manually set package name in import.ts

--- src/import.ts
+++ src/import.ts
@@ -1,5 +1,4 @@
 import { createImport } from 'auto-import-helper'
-import pkg from '../package.json'
 
 /** auto-import-helper */
-export default createImport(pkg.name, ['at', 'createMeta', 'createPromiseQueue', 'groupBy', 'isArray', 'isBoolean', 'isDate', 'isDef', 'isFunction', 'isInRange', 'isNull', 'isNumber', 'isObject', 'isPromise', 'isRegExp', 'isString', 'isUndefined', 'noop', 'pipe', 'sleep', 'toArray', 'toLF', 'toNumber', 'toPromise', 'uniq'] as const)
+export default createImport('@s3xysteak/utils', ['at', 'createMeta', 'createPromiseQueue', 'groupBy', 'isArray', 'isBoolean', 'isDate', 'isDef', 'isFunction', 'isInRange', 'isNull', 'isNumber', 'isObject', 'isPromise', 'isRegExp', 'isString', 'isUndefined', 'noop', 'pipe', 'sleep', 'toArray', 'toLF', 'toNumber', 'toPromise', 'uniq'] as const)
