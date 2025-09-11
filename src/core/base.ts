65d5dd924c14186014ae4612d58a6b3a462588c8
s3xysteak <s3xysteak@outlook.com>
2025-09-11 14:34:41 +0800

    feat: nonNullable

--- src/core/base.ts
+++ src/core/base.ts
@@ -38,3 +38,12 @@ export function createMeta<T = any>(metaSymbol: PropertyKey = Symbol('createMeta
     [set, get] as const,
   )
 }
+
+/**
+ * Filter out null and undefined values, with correct type.
+ * @example
+ * [1, 2, null, 3].filter(nonNullable) // -> [1, 2, 3]
+ */
+export function nonNullable<T>(value: T): value is NonNullable<T> {
+  return value !== null && value !== undefined
+}
