0ffd4c8ac0d4be8d3f8b291795eb13e0f0ef6b7e
s3xysteak <s3xysteak@outlook.com>
2025-09-11 14:37:25 +0800

    feat: randomInt

--- src/core/number.ts
+++ src/core/number.ts
@@ -4,3 +4,15 @@ export function toNumber(raw: string | number) {
 }
 
 export const isInRange = (val: number, [min, max]: [number, number]) => val >= min && val <= max
+
+/**
+ * Closed interval random number
+ * @example
+ * randomInt(1, 10) // 1 <= x <= 10
+ * randomInt(1, 10, () => myRand()) // to replace `Math.random`
+ */
+export function randomInt(min: number, max: number, rand = () => Math.random()): number {
+  const low = Math.ceil(min)
+  const high = Math.floor(max)
+  return Math.floor(rand() * (high - low + 1)) + low
+}
